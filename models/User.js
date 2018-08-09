8836409dd9d431fe4bcb854cfba0c30d16aa5709
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-09 00:42:50 -0400

    Created the User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  friends: [
+    {
+      friend: this,
+      status: {
+        type: String
+      },
+      date: {
+        type: Date
+      }
+    }
+  ]
+});
