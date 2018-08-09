5f5491b08094c108c99dab7a7fdaa9082833b0b2
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-09 15:54:02 -0400

    Created a secret

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Shawn:Password123@ds153719.mlab.com:53719/private-data"
+  mongoURI: "mongodb://Shawn:Password123@ds153719.mlab.com:53719/private-data",
+  jwtSecret: "pUJd7LFwNHYPVTmj6vbeGTQy0Lwyej1lXi3xqn9P"
 };
