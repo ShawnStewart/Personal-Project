71e31ad5c3fd89eed9ab67ef3bd572ef5d1abe78
Alec Richardson <alec.richardson@mail.missouri.edu>
2018-08-21 18:58:35 -0500

    Worked on layout more
    Css grid not behaving as expected

--- my-app/src/Components/Board/Board.js
+++ my-app/src/Components/Board/Board.js
@@ -10,30 +10,59 @@ export default class BoardTop extends Component {
   render() {
     return (
       <div className="Board">
-        <div className="Board__top Board__style">
-          <div className="Board__tile Corner__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-        </div>
+        <div className="Grid__container">
+          <div className="Board__top Board__style">
+            <div className="Board__tile__across Corner__tile">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across Corner__tile">test</div>
+          </div>
 
-        <div className="Board__right">
-          <div className="Board__tile Corner__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
-          <div className="Board__tile">test</div>
+          <div className="Board__left">
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+          </div>
+
+          <div className="Board__right">
+            <div className="Board__tile__side Corner__tile">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+            <div className="Board__tile__side">test</div>
+          </div>
+
+          <div className="Board__bottom Board__style">
+            <div className="Board__tile__across Corner__tile">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across">test</div>
+            <div className="Board__tile__across Corner__tile">test</div>
+          </div>
         </div>
       </div>
     );
